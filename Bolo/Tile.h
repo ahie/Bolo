22b02003a6a7d0796ed60878fd41a9c2b6bc5172
ahie <aleksi.hietanen@tut.fi>
2014-10-06 21:38:23 +0300

    Tiles

--- Bolo/Tile.h
+++ Bolo/Tile.h
@@ -1,8 +1,26 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+const int TILE_TEXTURE_HEIGHT = 192;
+const int TILE_TEXTURE_WIDTH = 128;
+const int TILE_HEIGHT = 64;
+const int TILE_WIDTH = TILE_TEXTURE_WIDTH / 2;
+
 class Tile
 {
 public:
-	Tile();
+	Tile(float x, float y, sf::Texture* texture, int offsetX, int offsetY);
 	~Tile();
-};
+	virtual void render(sf::RenderWindow& window);
 
+private:
+	sf::Texture* texture;
+	sf::Sprite sprite;
+	float x;
+	float y;
+
+	struct SubtileInfo {
+		bool walkable;
+	};
+	SubtileInfo subtile[4];
+};
