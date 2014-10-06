305a08f78458a1b28c55db4a5b19bf6c1544953e
ahie <aleksi.hietanen@tut.fi>
2014-10-06 21:39:57 +0300

    main

--- Bolo/main.cpp
+++ Bolo/main.cpp
@@ -1,10 +1,44 @@
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <random>
+#include "Tile.h"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Marjaisa");
+	sf::Image image;
+	sf::Texture texture;
+	image.loadFromFile("C:\\Users\\Aleksi\\Desktop\\MM\\Marjaisa\\Marjaisa\\Resources\\Tilesets\\Cave.png");
+	image.createMaskFromColor(sf::Color(255, 255, 255), 0);
+	texture.loadFromImage(image);
+
+	std::vector<Tile> tiles;
+	for (int i = 0; i < 50; i++)
+	{
+		for (int j = 0; j < 50; j++)
+		{
+			if (i == 0 && j == 0)
+				tiles.push_back(Tile((float)i*TILE_WIDTH, (float)j*TILE_HEIGHT, &texture, 10, 0));
+			else if (j == 0)
+				tiles.push_back(Tile((float)i*TILE_WIDTH, (float)j*TILE_HEIGHT, &texture, 9, 0));
+			else if (i == 0)
+				tiles.push_back(Tile((float)i*TILE_WIDTH, (float)j*TILE_HEIGHT, &texture, 8, 0));
+
+			else if (rand() % 20 < 1)
+				tiles.push_back(Tile((float)i*TILE_WIDTH, (float)j*TILE_HEIGHT, &texture, 11, 2));
+			else if (rand() % 20 < 1)
+				tiles.push_back(Tile((float)i*TILE_WIDTH, (float)j*TILE_HEIGHT, &texture, 13, 2));
+			else if (rand() % 20 < 3)
+				tiles.push_back(Tile((float)i*TILE_WIDTH, (float)j*TILE_HEIGHT, &texture, 6, 0));
+			else
+				tiles.push_back(Tile((float)i*TILE_WIDTH, (float)j*TILE_HEIGHT, &texture, 6, 5));
+
+		}
+	}
+
+	sf::View view(sf::Vector2f(0, 250), sf::Vector2f(800, 600));
+	window.setView(view);
 
 	while (window.isOpen())
 	{
@@ -16,7 +50,12 @@ int main()
 		}
 
 		window.clear();
-		window.draw(shape);
+
+		for each (auto t in tiles)
+		{
+			t.render(window);
+		}
+
 		window.display();
 	}
 
