bb00e346766d3f91c9815c7eb8f1fe07f68aa231
ahie <aleksi.hietanen@tut.fi>
2014-10-07 23:35:19 +0300

    Pathing

--- Bolo/main.cpp
+++ Bolo/main.cpp
@@ -1,4 +1,21 @@
+#include "World.h"
+
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Bolo");
+	World world;
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear(sf::Color::Black);
+		world.render(window);
+		window.display();
+	}
+
 	return 0;
 }
