13f3073558d8bbf44c7d70213f88c3932102deeb
ahie <aleksi.hietanen@tut.fi>
2014-10-08 19:04:19 +0300

    MovingState fixed

--- Bolo/main.cpp
+++ Bolo/main.cpp
@@ -1,9 +1,11 @@
 #include "World.h"
+#include <SFML\System\Clock.hpp>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Bolo");
 	World world;
+	sf::Clock deltaClock;
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -11,8 +13,11 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			world.handleInput(event);
 		}
 		window.clear(sf::Color::Black);
+		sf::Time dt = deltaClock.restart();
+		world.update(window, dt.asSeconds());
 		world.render(window);
 		window.display();
 	}
